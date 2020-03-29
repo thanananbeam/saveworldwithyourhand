fcf3659bcdeecd32bfe708963f7e4fe6f93991be
ment12835 <ment12835@gmail.com>
2020-03-30 02:06:10 +0700

    import js and css

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,15 @@
 const express = require('express')
 var router = express.Router();
 
+var scriptsABC = [{ script: '/assets/home.js' }];
+var styleABC = [{ style: '/assets/home.css' }];
+
 router.get("/index/", (req, res) => {
-  res.render("home/index")
+  res.render("home/index", {
+    title: 'test polo',
+    scripts: scriptsABC,
+    styles: styleABC
+  })
 })
 
 module.exports = router;
