3991a4ad44848acfd7ff1eb1de86748feecae330
thanananbeam <thanananbeam@gmail.com>
2020-04-06 16:05:42 +0700

    change port 5000

--- server.js
+++ server.js
@@ -1,48 +1,50 @@
-require('./models/db')
+require("./models/db");
 
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const bodyparse = require('body-parser')
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyparse = require("body-parser");
 
-const employeeController = require('./controllers/employeeController')
-const homeController = require('./controllers/homeController')
-const poloController = require('./controllers/poloController') // คืออะไร?
-const covidController = require('./controllers/covidController')
-const mentController = require('./controllers/mentController')
-
+const employeeController = require("./controllers/employeeController");
+const homeController = require("./controllers/homeController");
+const poloController = require("./controllers/poloController"); // คืออะไร?
+const covidController = require("./controllers/covidController");
+const mentController = require("./controllers/mentController");
 
 var app = express();
-app.use(bodyparse.urlencoded({
-  extended: true
-}))
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyparse.json())
-app.set('views', path.join(__dirname, '/views/'))
+app.use(
+  bodyparse.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(bodyparse.json());
+app.set("views", path.join(__dirname, "/views/"));
 
-app.engine('hbs', exphbs({
-  extname: 'hbs',
-  defaultLayout: 'mainLayout',
-  layoutsDir: __dirname + "/views/layouts/"
-}))
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
 
-app.set('view engine', 'hbs')
+app.set("view engine", "hbs");
 
-var port = process.env.PORT
+var port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('express server started at port : ${port}')
-})
-
+  console.log("express server started at port : ${port}");
+});
 
 // Register app
-app.use('/home', homeController)
-app.use('/polo', poloController) // คืออะไร?
-app.use('/covid', covidController)
-app.use('/employee', employeeController)
-app.use('/ment', mentController)
-
+app.use("/home", homeController);
+app.use("/polo", poloController); // คืออะไร?
+app.use("/covid", covidController);
+app.use("/employee", employeeController);
+app.use("/ment", mentController);
 
 //default
-app.get('/', function (req, res) {
-  res.redirect('/home/index');
+app.get("/", function (req, res) {
+  res.redirect("/home/index");
 });
